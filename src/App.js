f770dd39f9347c50e19bba4fb115ffd04da35b31
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-08-16 16:22:28 +0500

    lect 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Ali";
 function App() {
+  let name = "Hassan";
+  name = "<b>Hassan</b>";
   return (
     <>
-      
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Sign Up</li>
+        </ul>
+      </nav>
+      <div className="containter">
+        <h2>
+          Hello {name}                          {/* JSX */}
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore hic, officiis similique mollitia tempora error rem at incidunt, voluptate, culpa magnam esse eius blanditiis temporibus neque animi. Eveniet, libero at.</p>
+      </div>
     </>
   );
 }
