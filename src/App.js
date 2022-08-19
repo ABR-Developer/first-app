7443315f833559dc771ec3de06cb235dd1fb47b3
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-08-19 08:31:27 +0500

    props and propType (incomplete)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">Link</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/">Action</a>
-              <a className="dropdown-item" href="/">Another action</a>
-              <div className="dropdown-divider"></div>
-              <a className="dropdown-item" href="/">Something else here</a>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+      <NavBar title="LOGO HERE" aboutText="About"/>             {/* passing props to component */}
+      {/* <NavBar />                                                not passing props to component 1 - defaultProps will  be used here , 2 - if there is no default props then component will be empty , 3 - if there is not default props and any prop is required then error will occur */}
+      {/* <NavBar title={3} aboutText="About"/>                     error because title's propType donot match */}
+      {/* <NavBar title="LOGO HERE" aboutText={5}/>                 error because aboutText's propType donot match */}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
-    </nav>
     </>
   );
 }
